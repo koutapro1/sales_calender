044fdd45dc0a50ddd43bd6d7bfb40b2c6186d712
Kota Tohata <koutapro1@gmail.com>
2022-01-18 19:07:34 +0900

    add: マイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_085641) do
+ActiveRecord::Schema.define(version: 2022_01_18_095539) do
+
+  create_table "sales", force: :cascade do |t|
+    t.integer "sales"
+    t.text "memo"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sales_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
