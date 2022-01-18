6932b7cb55b00e8ebe28c93586ef13d6220e39b8
Kota Tohata <koutapro1@gmail.com>
2022-01-18 18:28:10 +0900

    add: sales コントローラーとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'sales/index'
+  get 'sales/show'
+  get 'sales/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
