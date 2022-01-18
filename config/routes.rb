2bdcd350d39c4232dc0f18455d951e0923e5839f
Kota Tohata <koutapro1@gmail.com>
2022-01-18 22:17:47 +0900

    add: user_session コントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  get '/login', to: 'user_sessions#new', as: :login
+  post '/login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
+
   resources :users
   resources :sales
 end
