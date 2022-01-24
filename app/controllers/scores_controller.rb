c5d888dd621efbe3303c11f260af9f7dc8fe1249
Kota Tohata <koutapro1@gmail.com>
2022-01-24 14:12:55 +0900

    add: 売上を登録するcreateメソッドを追加

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -9,4 +9,20 @@ class ScoresController < ApplicationController
   def new
     @score = Score.new
   end
+
+  def create
+    @score = Score.new(score_params)
+    if @score.save
+      redirect_to scores_path, success: '売上を登録しました'
+    else
+      flash.now[:warning] = '売上を登録できませんでした'
+      render :new
+    end
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:score, :date, :memo).merge(user_id: current_user.id)
+  end
 end
