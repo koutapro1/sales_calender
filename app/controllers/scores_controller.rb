b367180228d294106dadd3fd88087b27954fde77
Kota Tohata <koutapro1@gmail.com>
2022-02-10 18:16:03 +0900

    improve: 先月度、今月度の売上の検索処理をコントローラーからモデルに移動

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,24 +2,8 @@ class ScoresController < ApplicationController
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
     @scores = current_user.scores.all.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    @scores_in_previous_month = if start_date.month == 1
-                                  current_user.scores.all.where(start_time: "#{start_date.year - 1}-12-18"..."#{start_date.year}-01-16 23:59:59")
-                                elsif start_date.month == 2
-                                  current_user.scores.all.where(start_time: "#{start_date.year}-01-17"..."#{start_date.year}-02-15 23:59:59")
-                                elsif start_date.month == 3
-                                  current_user.scores.all.where(start_time: "#{start_date.year}-02-16"..."#{start_date.year}-03-17 23:59:59")
-                                else
-                                  current_user.scores.all.where(start_time: "#{start_date.year}-#{start_date.month - 1}-18"..."#{start_date.year}-#{start_date.month}-17 23:59:59")
-                                end
-    @scores_in_next_month = if start_date.month == 1
-                              current_user.scores.all.where(start_time: "#{start_date.year}-01-17"..."#{start_date.year}-02-15 23:59:59")
-                            elsif start_date.month == 2
-                              current_user.scores.all.where(start_time: "#{start_date.year}-02-16"..."#{start_date.year}-03-17 23:59:59")
-                            elsif start_date.month == 3
-                              current_user.scores.all.where(start_time: "#{start_date.year}-03-18"..."#{start_date.year}-04-17 23:59:59")
-                            else
-                              current_user.scores.all.where(start_time: "#{start_date.year}-#{start_date.month}-18"..."#{start_date.year}-#{start_date.month + 1}-17 23:59:59")
-                            end
+    @scores_in_previous_month = Score.set_scores_in_previous_month(start_date, current_user)
+    @scores_in_next_month = Score.set_scores_in_next_month(start_date, current_user)
   end
 
   def show
--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,28 @@ class Score < ApplicationRecord
 
   validates :score, presence: true
   validates :start_time, presence: true
+
+  def self.set_scores_in_previous_month(start_date, current_user)
+    if start_date.month == 1
+      self.where(user_id: current_user.id, start_time: "#{start_date.year - 1}-12-18"..."#{start_date.year}-01-16 23:59:59")
+    elsif start_date.month == 2
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-01-17"..."#{start_date.year}-02-15 23:59:59")
+    elsif start_date.month == 3
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-02-16"..."#{start_date.year}-03-17 23:59:59")
+    else
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-#{start_date.month - 1}-18"..."#{start_date.year}-#{start_date.month}-17 23:59:59")
+    end
+  end
+
+  def self.set_scores_in_next_month(start_date, current_user)
+    if start_date.month == 1
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-01-17"..."#{start_date.year}-02-15 23:59:59")
+    elsif start_date.month == 2
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-02-16"..."#{start_date.year}-03-17 23:59:59")
+    elsif start_date.month == 3
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-03-18"..."#{start_date.year}-04-17 23:59:59")
+    else
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-#{start_date.month}-18"..."#{start_date.year}-#{start_date.month + 1}-17 23:59:59")
+    end
+  end
 end
