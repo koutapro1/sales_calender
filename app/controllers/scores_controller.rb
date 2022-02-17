3c4462da9d64eac05b7493ccc9abe2e8de3ccca0
Kota Tohata <koutapro1@gmail.com>
2022-02-17 14:24:32 +0900

    add: 売上を削除する機能を追加 & 戻るボタンを追加

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,6 @@
 class ScoresController < ApplicationController
+  before_action :set_score, only: [:show, :destroy]
+
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
     @scores = current_user.scores.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
@@ -6,9 +8,7 @@ class ScoresController < ApplicationController
     @scores_in_next_month = Score.get_scores_in_next_month(start_date, current_user)
   end
 
-  def show
-    @score = Score.find(params[:id])
-  end
+  def show; end
 
   def new
     @score = Score.new
@@ -24,9 +24,18 @@ class ScoresController < ApplicationController
     end
   end
 
+  def destroy
+    @score.destroy!
+    redirect_to scores_path, success: '売上を削除しました'
+  end
+
   private
 
   def score_params
     params.require(:score).permit(:score, :start_time, :memo).merge(user_id: current_user.id)
+  end
+
+  def set_score
+    @score = Score.find(params[:id])
   end
 end
