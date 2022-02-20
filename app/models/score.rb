2fbac01f56db040e850f942b6bf8d4c6564404d0
Kota Tohata <koutapro1@gmail.com>
2022-02-20 17:21:23 +0900

    add: カレンダーに表示される日付の売上を取得するメソッドを追加

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,27 +4,19 @@ class Score < ApplicationRecord
   validates :score, presence: true
   validates :start_time, presence: true
 
-  def self.get_scores_in_previous_month(start_date, current_user)
-    if start_date.month == 1
-      self.where(user_id: current_user.id, start_time: "#{start_date.year - 1}-12-18"..."#{start_date.year}-01-16 23:59:59")
-    elsif start_date.month == 2
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-01-17"..."#{start_date.year}-02-15 23:59:59")
-    elsif start_date.month == 3
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-02-16"..."#{start_date.year}-03-17 23:59:59")
-    else
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-#{start_date.month - 1}-18"..."#{start_date.year}-#{start_date.month}-17 23:59:59")
-    end
-  end
-
-  def self.get_scores_in_next_month(start_date, current_user)
-    if start_date.month == 1
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-01-17"..."#{start_date.year}-02-15 23:59:59")
-    elsif start_date.month == 2
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-02-16"..."#{start_date.year}-03-17 23:59:59")
-    elsif start_date.month == 3
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-03-18"..."#{start_date.year}-04-17 23:59:59")
-    else
-      self.where(user_id: current_user.id, start_time: "#{start_date.year}-#{start_date.month}-18"..."#{start_date.year}-#{start_date.month + 1}-17 23:59:59")
+  def self.get_scores_in_current_month(start_date, current_user)
+    if start_date >= Date.new(start_date.year, 12, 18) && start_date <= Date.new(start_date.year, 12, 31)
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-12-18".."#{start_date.year + 1}-01-16")
+    elsif start_date >= Date.new(start_date.year, 1, 1) && start_date <= Date.new(start_date.year, 1, 16)
+      self.where(user_id: current_user.id, start_time: "#{start_date.year - 1}-12-18".."#{start_date.year}-01-16")
+    elsif start_date >= Date.new(start_date.year, 1, 17) && start_date <= Date.new(start_date.year, 2, 15)
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-01-17".."#{start_date.year}-02-15")
+    elsif start_date >= Date.new(start_date.year, 2, 16) && start_date <= Date.new(start_date.year, 3, 17)
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-02-16".."#{start_date.year}-03-17")
+    elsif start_date >= Date.new(start_date.year, start_date.month, 18)
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-#{start_date.month}-18".."#{start_date.year}-#{start_date.month + 1}-17")
+    elsif start_date <= Date.new(start_date.year, start_date.month, 18)
+      self.where(user_id: current_user.id, start_time: "#{start_date.year}-#{start_date.month - 1}-18".."#{start_date.year}-#{start_date.month}-17")
     end
   end
 end
