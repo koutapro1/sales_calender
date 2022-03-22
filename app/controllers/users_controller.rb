741f66dd23a39afaeda7eb64f03c924b660d89fd
Kota Tohata <koutapro1@gmail.com>
2022-03-22 15:32:54 +0900

    add: show, edit, updateアクションを追加 #13
    add: 他人からのアクセスを制限する処理を追加 #13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
   before_action :already_logged_in, only: [:new]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :forbid_access_to_other_user, only: [:show, :edit, :update]
   
   def new
     @user = User.new
   end
+
+  def show; end
   
   def create
     @user = User.new(user_params)
@@ -16,7 +20,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, success: 'ユーザー情報を更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def forbid_access_to_other_user
+    redirect_to root_path, danger: '権限がありません' if @user != current_user
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :email_confirmation, :password, :password_confirmation)
