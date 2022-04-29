ffb6546101258bb280d7cbf433152ed9c29fa4a5
Kota Tohata <koutapro1@gmail.com>
2022-04-29 21:21:30 +0900

    add: score_detail関連のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'scores#index'
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   get '/login', to: 'user_sessions#new', as: :login
   post '/login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy', as: :logout
@@ -15,5 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :users,  only: [:new, :show, :create, :edit, :update]
-  resources :scores, only: [:index, :new, :create, :destroy]
+  resources :scores, only: [:index, :new, :create, :destroy] do
+    resources :score_details, only: [:index, :show, :create]
+  end
 end
