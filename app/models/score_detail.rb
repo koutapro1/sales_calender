1112909f5b04ab878cf6124b24c74a6a39bb5412
Kota Tohata <koutapro1@gmail.com>
2022-05-11 14:18:36 +0900

    add: 緯度経度をroads apiで使用できるフォーマットに返還するインスタンスメソッドを追加

--- app/models/score_detail.rb
+++ app/models/score_detail.rb
@@ -1,4 +1,11 @@
 class ScoreDetail < ApplicationRecord
   belongs_to :score
   serialize :coords, Array
+
+  def translate_for_google_map
+    coords = self.coords
+    coords = coords.each_slice(2).to_a
+    result = coords.map { |coord| coord.join(",") }
+    result.join("|")
+  end
 end
