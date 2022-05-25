d7404ea18fed0b5ce0e63b6898abd465459f7988
Kota Tohata <koutapro1@gmail.com>
2022-05-25 14:49:01 +0900

    add: score_detailsテーブルにfareカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_29_101806) do
+ActiveRecord::Schema.define(version: 2022_05_25_054709) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_04_29_101806) do
     t.integer "score_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "fare", null: false
     t.index ["score_id"], name: "index_score_details_on_score_id"
   end
 
