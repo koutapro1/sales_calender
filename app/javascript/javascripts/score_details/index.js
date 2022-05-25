315c029e45b985298d649ff8b40d7751e12d0480
Kota Tohata <koutapro1@gmail.com>
2022-05-25 14:27:51 +0900

    add: タクシーメーター機能を追加
    進んだ距離や時間から料金を計算する機能を追加

--- app/javascript/javascripts/score_details/index.js
+++ app/javascript/javascripts/score_details/index.js
@@ -1,10 +1,12 @@
-import { format } from 'date-fns'
+import { differenceInQuarters, format } from 'date-fns'
+const getHaversineDistance = require('get-haversine-distance')
+
 function initMap() {
-  const timeStamps = [];
-  const coordinates = [];
   const scoreId = gon.score.id;
   const pickupTiming = "pickup";
   const dropoffTiming = "dropoff";
+  let timeStamps = [];
+  let coordinates = [];
   let watchId;
   let pickupLatLng;
   let dropoffLatLng;
@@ -12,6 +14,9 @@ function initMap() {
   let dropoffTime;
   let pickupAddress;
   let dropoffAddress;
+  let fare = 420;
+  let meterDistance = 0;
+  let oneMeter = true;
 
   $('#pickup').click( () => {
     watchId = navigator.geolocation.watchPosition(
@@ -25,28 +30,58 @@ function initMap() {
 
   $("#dropoff").click( () => {
     navigator.geolocation.clearWatch(watchId);
+    pickupLatLng = new google.maps.LatLng(coordinates[0], coordinates[1]);
+    dropoffLatLng = new google.maps.LatLng(coordinates[coordinates.length - 2], coordinates[coordinates.length - 1]);
+    pickupTime = timeStamps[0];
+    dropoffTime = timeStamps[timeStamps.length - 1];
     GeocodingFunc(pickupLatLng, pickupTiming, GeocodingFunc);
   })
 
   function success (data) {
     setTimestamp(data);
     coordinates.push(data.coords.latitude, data.coords.longitude);
-    pickupLatLng = new google.maps.LatLng(coordinates[0], coordinates[1]);
-    dropoffLatLng = new google.maps.LatLng(coordinates[coordinates.length - 2], coordinates[coordinates.length - 1]);
-    pickupTime = timeStamps[0];
-    dropoffTime = timeStamps[timeStamps.length - 1];
-    console.log(data);
-    console.log(coordinates);
-    console.log(timeStamps);
-    console.log(pickupTime);
-    console.log(dropoffTime);
+    calcFare();
+    // console.log(data);
+    // console.log(coordinates);
+    // console.log(timeStamps);
   }
 
   function setTimestamp (data) {
     let time = data.timestamp;
     let dateObj = new Date(time);
-    let timeStr = format(dateObj, 'yyyy-MM-dd HH:mm:ss.SSS');
-    timeStamps.push(timeStr);
+    timeStamps.push(dateObj);
+  }
+
+  function calcFare () {
+    if (coordinates.length > 3) {
+      let a = { lat: coordinates[coordinates.length - 4], lng: coordinates[coordinates.length - 3] }
+      let b = { lat: coordinates[coordinates.length - 2], lng: coordinates[coordinates.length - 1] }
+      let distance = getHaversineDistance(a,b) * 1000     //メートル
+      let duration = (new Date(timeStamps[timeStamps.length - 1]) - new Date(timeStamps[timeStamps.length - 2])) / 1000   //秒
+      let speed = (distance / duration) * 60 * 60 / 1000    //毎時キロ
+
+      if (speed <= 10) {
+        meterDistance += duration * (233 / 85);               // 時間メーター
+      } else if (speed >= 10) {
+        meterDistance += getHaversineDistance(a,b) * 1000;    // 距離メーター
+      }
+      if(meterDistance > 1052) {                              // ワンメーター超えたときの処理
+        fare += 80;
+        meterDistance -= 1052;
+        oneMeter = false;
+      }
+      if (oneMeter ==false && meterDistance > 233) {          // ワンメーター以降の計算
+        let counter = Math.floor(meterDistance / 233)
+        fare += counter * 80;
+        meterDistance = meterDistance % 233;
+      }
+      console.log("時間:" + duration + "秒");
+      console.log("距離:" + distance + "m");
+      console.log("時速" + speed + "km")
+    }
+    console.log("メーター距離:" + meterDistance + "m");
+    console.log(fare + "円");
+    $('#meter').html(`<div>${fare} 円</div>`);
   }
 
   function GeocodingFunc(latlng, timing, callback) {
@@ -84,6 +119,15 @@ function initMap() {
     )
   }
 
+  function resetMeter () {
+    timeStamps = [];
+    coordinates = [];
+    fare = 420;
+    meterDistance = 0;
+    oneMeter = true;
+    $('#meter').html("<div>0 円</div>");
+  }
+
   function ajaxScoreDetail () {
     $.ajax({
       url: `/scores/${scoreId}/score_details`,
@@ -104,6 +148,7 @@ function initMap() {
       $('.lists').append(
         `<li><a href="/scores/${scoreId}/score_details/${data.id}"> ${data.pickup_address} ~ ${data.dropoff_address}</a> </li>`
         )
+      resetMeter();
     })
     .fail(function() {
       alert("error");
