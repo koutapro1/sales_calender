bad0b3ddb30909b14feec16b9f5f23a577563b77
Kota Tohata <koutapro1@gmail.com>
2022-06-08 11:36:30 +0900

    add: 売上詳細を削除するボタンを追加し、それに対応するdestroyアクションを追加

--- app/controllers/score_details_controller.rb
+++ app/controllers/score_details_controller.rb
@@ -1,6 +1,6 @@
 class ScoreDetailsController < ApplicationController
   protect_from_forgery
-  before_action :set_score, only: [:index, :show]
+  before_action :set_score, only: [:index, :show, :destroy]
   
   def index
     @score_details = @score.score_details
@@ -21,6 +21,11 @@ class ScoreDetailsController < ApplicationController
     gon.score_detail = @score_detail
     gon.translated_coordinates = @score_detail.translate_for_google_map
     gon.google_map_api_key = ENV['GOOGLE_MAP_API_KEY']
+  end
+
+  def destroy
+    @score_detail = @score.score_details.find(params[:id])
+    @score_detail.destroy!
   end
 
   private
