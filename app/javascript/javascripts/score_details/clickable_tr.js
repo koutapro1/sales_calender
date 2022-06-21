794a90c2885de4b3c8d0dd3a401a38188a0ef073
Kota Tohata <koutapro1@gmail.com>
2022-06-21 17:15:46 +0900

    add: 売上詳細の編集と削除をドロップダウンで表示する機能をzissou

--- app/javascript/javascripts/score_details/clickable_tr.js
+++ app/javascript/javascripts/score_details/clickable_tr.js
@@ -1,3 +1,15 @@
-$(".score-detail-tr").on('click', function () {
+$(document).on('click', '.score-detail-tr', function () {
   window.location = $(this).data("href");
 });
+
+$(document).on('click', '.menus', function (e) {
+  e.stopPropagation();
+  $(this).find('.dropdown-menu').toggleClass('show');
+  $('.menus').not($(this)).find('.dropdown-menu').removeClass('show');
+})
+
+$(document).on('click', function(e) {
+  if(!$(e.target).closest('.menus').length) {
+    $('.dropdown-menu').removeClass('show');
+  }
+})
