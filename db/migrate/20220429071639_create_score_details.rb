6bec259cb8001eefd6feacbbe1dac53a9143d0c2
Kota Tohata <koutapro1@gmail.com>
2022-06-27 15:33:46 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20220429071639_create_score_details.rb
+++ db/migrate/20220429071639_create_score_details.rb
@@ -1,12 +1,13 @@
 class CreateScoreDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :score_details do |t|
-      t.text :coords, array: true, null: false
-      t.string :pickup_address,    null: false
-      t.string :dropoff_address,   null: false
-      t.datetime :pickup_time,     null: false
-      t.datetime :dropoff_time,    null: false
-      t.references :score,         null: false, foreign_key: true
+      t.text       :coords,           null: false, array: true
+      t.string     :pickup_address,   null: false
+      t.string     :dropoff_address,  null: false
+      t.datetime   :pickup_time,      null: false
+      t.datetime   :dropoff_time,     null: false
+      t.integer    :fare,             null: false
+      t.references :score,            null: false, foreign_key: true
 
       t.timestamps
     end
