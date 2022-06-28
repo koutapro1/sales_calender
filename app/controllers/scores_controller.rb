09768326033c493818e4da2f82953b6896818079
Kota Tohata <koutapro1@gmail.com>
2022-06-28 14:16:55 +0900

    refactor: 頻出する記述をbefore_actionで定義

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,7 @@
 class ScoresController < ApplicationController
-  before_action :set_score, only: [:edit, :update, :destroy]
+  before_action :set_score, only: %i[edit update destroy]
+  before_action :set_start_date, only: %i[create edit update destroy]
+  before_action :set_scores_in_current_month, only: %i[create update destroy]
 
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
@@ -8,27 +10,19 @@ class ScoresController < ApplicationController
   end
 
   def create
-    @start_date = params[:start_date].to_date
-    @scores_in_current_month = current_user.scores.get_scores_in_current_month(@start_date)
     @success_message = "売上を登録しました"
     @score = current_user.scores.build(score_params)
     @score.save
   end
 
-  def edit
-    @start_date = params[:start_date].to_date
-  end
+  def edit; end
 
   def update
-    @start_date = params[:start_date].to_date
-    @scores_in_current_month = current_user.scores.get_scores_in_current_month(@start_date)
     @success_message = "売上を変更しました"
     @score.update(score_params)
   end
 
   def destroy
-    start_date = params[:start_date].to_date
-    @scores_in_current_month = current_user.scores.get_scores_in_current_month(start_date)
     @success_message = "売上を削除しました"
     @score.destroy!
   end
@@ -41,5 +35,13 @@ class ScoresController < ApplicationController
 
   def set_score
     @score = Score.find(params[:id])
+  end
+
+  def set_start_date
+    @start_date = params[:start_date].to_date
+  end
+
+  def set_scores_in_current_month
+    @scores_in_current_month = current_user.scores.get_scores_in_current_month(@start_date)
   end
 end
