24c3b4232f8a20d62743eeaa28b02fb6586e19ca
Kota Tohata <koutapro1@gmail.com>
2022-07-02 23:34:48 +0900

    refactor: eslintでリファクタリング

--- app/javascript/javascripts/score.js
+++ app/javascript/javascripts/score.js
@@ -1,52 +1,53 @@
-$(function () {
+$(() => {
   // ---------選んだ日付の売上詳細、又は売上追加フォームを表示するための処理----------
 
   // クリックした日付の日付を取得して、ajaxで送信
-  $('.table td').click(function() {
-    const startTime = getStartTime($(this));
+  const getStartTime = (selector) => {
+    const classes = $(selector).attr('class').split(' ');
+    return classes[1];
+  };
+
+  const getStartDate = () => {
+    const classes = $('.start-date').attr('class').split(' ');
+    return classes[1];
+  };
+
+  $('.table td').on('click', (e) => {
+    const startTime = getStartTime($(e.currentTarget));
     const startDate = getStartDate();
     if ($('.js-searched-score').length) {
-      $('.js-searched-score').remove()
-    };
+      $('.js-searched-score').remove();
+    }
 
     $.ajax({
       type: 'GET',
       url: '/scores/searches/score.html',
       data: {
         start_time: startTime,
-        start_date: startDate
-      }
+        start_date: startDate,
+      },
     })
-    .done(function (data) {
-      $(".js-searched-score-field").html(data)
-    })
+      .done((data) => {
+        $('.js-searched-score-field').html(data);
+      });
   });
-
-  const getStartTime = (selector) => {
-    const classes = $(selector).attr('class').split(" ");
-    return classes[1];
-  };
-
-  const getStartDate = () => {
-    const classes = $('.start-date').attr('class').split(" ");
-    return classes[1];
-  };
 
   // ----------月度ジャンプの開閉
-  $(document).on("click", '.js-toggle-button, .calendar-title', function(){
-    $('.js-month-jump').slideToggle(200, alertFunc);
-  });
-  function alertFunc() {
-    if($(this).css('display') == 'block') {
+  const toggleFunc = () => {
+    if ($('.js-month-jump').css('display') === 'block') {
       $('.js-toggle-button').removeClass('fas fa-caret-down');
       $('.js-toggle-button').addClass('fas fa-caret-up');
-      $('.calendar-heading').css('box-shadow', '')
-      $('.js-month-jump').css({'box-shadow': '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.05)'});
-    }else{
+      $('.calendar-heading').css('box-shadow', '');
+      $('.js-month-jump').css({ 'box-shadow': '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.05)' });
+    } else {
       $('.js-toggle-button').removeClass('fas fa-caret-up');
       $('.js-toggle-button').addClass('fas fa-caret-down');
-      $('.js-month-jump').css('box-shadow', '')
-      $('.calendar-heading').css({'box-shadow': '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.05)'});
+      $('.js-month-jump').css('box-shadow', '');
+      $('.calendar-heading').css({ 'box-shadow': '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.05)' });
     }
   };
-})
+
+  $(document).on('click', '.js-toggle-button, .calendar-title', () => {
+    $('.js-month-jump').slideToggle(200, toggleFunc);
+  });
+});
